40bf314ad901278c75b73fc4808edd292d690e7a
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-05 09:57:19 +0300

    Debugging

--- w7-shortest-knight-path/aleksis.rb
+++ w7-shortest-knight-path/aleksis.rb
@@ -2,9 +2,14 @@ BRD = ("a".."h").map{ |c| ("1".."8").map{ c + _1 } }.flatten
 MVS = [1,2,-1,-2].permutation(2).select { (_1*_2).abs == 2 }
 
 def knight(s,f,d=1)
+  puts("s: #{s.inspect}")
   mvs = [s].
-    flatten.
+    flatten
+  puts("mvs: #{mvs.inspect}")
+  mvs = [s].
+    # flatten.
     map do |x|
+    puts("x: #{x.inspect}")
     MVS.map { _1.zip(x.chars.map(&:ord)).map(&:sum).map(&:chr) }.map(&:join)
   end.
     flatten.
@@ -14,3 +19,6 @@ def knight(s,f,d=1)
 
   knight(mvs,f,d+1)
 end
+
+puts("BRD: #{BRD.inspect}")
+knight("a1", "h8")
